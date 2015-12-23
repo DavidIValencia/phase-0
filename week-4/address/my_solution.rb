a0af6bb9e14db776389926aedbee2a9b1c60706f
David <David@Davids-MacBook-Air.local>
2015-12-22 22:12:02 -0800

    add 4.3

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,9 @@
+# Format an Address
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+def make_address(w, x, y, z)
+  "You live at #{w}, in the beautiful city of #{x}, #{y}. Your zip is #{z}."
+end
